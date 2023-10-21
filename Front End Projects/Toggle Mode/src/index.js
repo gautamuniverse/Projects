8c90abc255218c259cacc0bb3d3abd91a33697cd
gautamuniverse <64640338+gautamuniverse@users.noreply.github.com>
2023-10-21 17:58:18 +0530

    Delete Front End Projects directory

--- Front End Projects/Toggle Mode/src/index.js
+++
@@ -1,8 +0,0 @@
-var trigger = document.getElementById("triggerButton");
-trigger.addEventListener("click", function () {
-  var button = this;
-  button.classList.toggle("active");
-  document.body.classList.toggle("bgColor");
-  var textColor = document.getElementById("triggerText");
-  textColor.classList.toggle("white");
-});
--- Front End Projects/percentage-scrolled/src/index.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener("scroll", function () {
-  let currentScrolled = Math.ceil(window.scrollY);
-  let percentage = document.getElementsByClassName("percentage")[0];
-  percentage.textContent = currentScrolled + "%";
-});
